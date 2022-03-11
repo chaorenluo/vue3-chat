71333ae226c094a408711eb0d66e10de633d60df
罗佳明 <ljm@addcn.com>
2022-03-11 16:38:54 +0800

    初始化server端代码

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-    imports: [],
-    controllers: [AppController],
-    providers: [AppService],
+    imports: [
+        // TypeOrmModule.forRoot({
+        //     type: 'mysql',
+        //     port: 3306,
+        //     username: 'root',
+        //     password: '123456',
+        //     database: 'chat',
+        //     charset: 'utf8mb4', // 设置chatset编码为utf8mb4
+        //     autoLoadEntities: true,
+        //     synchronize: true,
+        // }),
+        UserModule,
+    ],
 })
 export class AppModule {}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { logger } from './common/middleware/logger.middleware';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { ResponseInterceptor } from './common/interceptor/response.interceptor';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    await app.listen(3300);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+    //全局中间件
+    app.use(logger);
+
+    // 全局异常过滤器
+    app.useGlobalFilters(new HttpExceptionFilter());
+
+    // 配置全局拦截器 拦截返回值
+    app.useGlobalInterceptors(new ResponseInterceptor());
+
+    // 配置静态资源
+    app.useStaticAssets(join(__dirname, '../public', '/'), {
+        prefix: '/',
+        setHeaders: (res) => {
+            res.set('Cache-Control', 'max-age=2592000');
+        },
+    });
+
+    await app.listen(3001);
 }
 bootstrap();
